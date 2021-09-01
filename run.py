88d0d50ab904fb85efb0d5218779493d1bb77194
hartnetl <laura.hartnett@outlook.com>
2021-09-01 16:09:41 +0000

    Create and link all 4 pages, introduce an unlinked bootstrap theme

--- run.py
+++ run.py
@@ -1,22 +1,37 @@
 import os
-from flask import Flask 
+from flask import Flask, render_template
 
 # create an instance of the class Flask and storing it in a variable called app
 # The first argument of the Flask class, is the name of the application's module - our package.
 # Since we're just using a single module, we can use __name__ which is a built-in Python variable.
 app = Flask(__name__)
 
+
 # @ is a decorator, which is a function wrapper
-# "/" Brings us to the root directory and returns hello world
+# "/" Brings us to the root directory and returns what is in the index function
 @app.route("/")
 def index():
-    return "Hello World"
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 
-if __name__ == "__main__": #main is the default module in python
-    app.run( #We run the app using these arguments
-        host=os.environ.get("IP", "0.0.0.0"), #Looks for environment's IP, returns default if none
-        port=int(os.environ.get("PORT", "5000")), #5000 is a commonly used port by flask
-        debug=True  #Allows for easier debugging when developing
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
+
+
+if __name__ == "__main__":     # main is the default module in python
+    app.run(               # We run the app using these arguments
+        host=os.environ.get("IP", "0.0.0.0"),        # Looks for environment's IP, returns default if none
+        port=int(os.environ.get("PORT", "5000")),    # 5000 is a commonly used port by flask
+        debug=True     # Allows for easier debugging when developing
         # SET DEBUG TO FALSE FOR PRODUCTION/ ASSESSMENT SUBMISSION
         )
